455b73580459192ccc0a047e8d35e3d9248a213a
nabiladigo <nabiladigo91@gmail.com>
2022-02-07 19:21:09 -0500

    create templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.views.generic.base import TemplateView # <- a class to handle sending a type of response
+
 
 # Create your views here.
+
+# Here we will be creating a class called Home and extending it from the View class
+class Home(TemplateView):
+    template_name = "home.html"
